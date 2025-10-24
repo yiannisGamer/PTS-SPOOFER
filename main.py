b064291f3d0806f64d5ac99414bea909aa2b8cc9
yiannisGamer <83921980+yiannisGamer@users.noreply.github.com>
2025-10-24 13:01:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ intents.members = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 # ---------- Config: βάλε τα δικά σου ----------
-STAFF_ROLES = [123456789012345678, 987654321098765432]  # βάλτες εδώ τα role IDs σου
+STAFF_ROLES = [1288087153997516913, 1289538235495878659, 1288090189255675944, 1288106262126657586]  # βάλτες εδώ τα role IDs σου
 THUMBNAIL_URL = "https://cdn.wallpapersafari.com/77/21/0QwLjm.jpg"     # άλλαξε με τη δική σου εικόνα
 EMBED_COLOR = discord.Color.red()
 EMBED_TITLE = "WELCOME TO PTS SUPPORT🔥"
