35fc6f72667686445ed51025eb31cf2713584489
yiannisGamer <83921980+yiannisGamer@users.noreply.github.com>
2025-10-25 01:00:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -70,11 +70,11 @@ async def ticket(ctx):
                 category = await guild.create_category("🎫 Tickets")
 
             # Παίρνουμε τι επέλεξε ο χρήστης
-            ticket_type = self.values[0]  # π.χ. "owner" ή "general"
+            ticket_type = self.values[0]  # π.χ. "🛒Buy A Product" ή "📞Support"
 
             # Ορισμός prefix ανάλογα με την επιλογή
             if "🛒Buy A Product" in ticket_type.lower():
-                prefix = "owner"
+                prefix = "🛒Buy A Product"
            elif "📞Support" in ticket_type.lower():
               prefix = "📞Support"
           else:
