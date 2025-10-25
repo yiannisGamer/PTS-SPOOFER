3ff9ed9799fe9dea7f89f126ff782e443baaf456
yiannisGamer <83921980+yiannisGamer@users.noreply.github.com>
2025-10-25 09:06:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -135,6 +135,12 @@ async def ticket(ctx):
                     color=discord.Color.red()  # Μπορείς να αλλάξεις το χρώμα
                 )
 
+                 # Βάζεις την εικόνα του χρήστη στο thumbnail
+                 embed.set_thumbnail(url=user.avatar.url)
+               
+                 # Προαιρετικά μπορείς να προσθέσεις footer με το username
+                 embed.set_footer(text=f"Ticket του χρήστη: {user.name}")
+
                 # Στέλνει το embed σε όλους στο κανάλι
                 await btn_interaction.channel.send(embed=embed)
 
