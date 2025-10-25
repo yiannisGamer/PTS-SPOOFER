0ca9a11890200c04f530ab8342154119336c6fc8
yiannisGamer <83921980+yiannisGamer@users.noreply.github.com>
2025-10-25 09:24:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -134,10 +134,16 @@ async def ticket(ctx):
                     description="Αυτό το ticket θα κλείσει σε 10 δευτερόλεπτα.",
                     color=discord.Color.red()  # Μπορείς να αλλάξεις το χρώμα
                 )
-               
+
+                 import datetime
+
+                 # Ελληνική ώρα (χωρίς να χρειάζεται pytz ή zoneinfo)
+                 current_time = datetime.datetime.utcnow() + datetime.timedelta(hours=3)
+                 time_str = current_time.strftime("%I:%M%p").lstrip("0")  # π.χ. 5:00AM
+
                 embed.set_footer(
-                text=f"{user.name} | Σήμερα στις {time_str}",
-                icon_url=user.display_avatar.url
+                    text=f"{user.name} | Σήμερα στις {time_str}",
+                    icon_url=user.display_avatar.url
                 )
 
                 # Στέλνει το embed σε όλους στο κανάλι
