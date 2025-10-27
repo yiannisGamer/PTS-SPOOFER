aceac34ee8cecca3a44c3599017104ba01d0faa1
yiannisGamer <83921980+yiannisGamer@users.noreply.github.com>
2025-10-27 08:21:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -307,7 +307,10 @@ async def ticket(ctx):
             # Παίρνουμε το label που επέλεξε ο χρήστης
             ticket_type = self.values[0]
             ticket_label = next(o.label for o in self.options if o.value == ticket_type)
-           
+            ticket_channel = await guild.create_text_channel(
+                channel_name, category=category, overwrites=overwrites, topic=f"Ticket για {user}"
+            )
+
             # --- Μήνυμα στο ίδιο κανάλι (όπως στη φωτό) ---
             if ticket_label == "📞Support":
                 await interaction.response.send_message(
