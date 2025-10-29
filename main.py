b8e96527711e17a8b1559f49cd5864f99d028153
yiannisGamer <83921980+yiannisGamer@users.noreply.github.com>
2025-10-29 09:28:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -77,12 +77,12 @@ async def sarck(ctx):
     # 🔹 Εικόνα μεγάλη κάτω (αν θες)
     embed.set_image(url="https://media.discordapp.net/attachments/1288996430383677450/1289343395025784863/Screenshot_20240928_004928_Chrome.jpg?ex=69028c1c&is=69013a9c&hm=cf2ef2b31140a8ee32f572c2ed48f1eec99c429e45ae030a3ef02a124a98fea8&=&format=webp&width=1523&height=864")  # μπορεί να είναι και GIF
     
-    embed2 = discord.Embed(color=discord.Color.red())
-    embed2.set_image(url="https://media.discordapp.net/attachments/1288996430383677450/1289343395025784863/Screenshot_20240928_004928_Chrome.jpg?ex=69028c1c&is=69013a9c&hm=cf2ef2b31140a8ee32f572c2ed48f1eec99c429e45ae030a3ef02a124a98fea8&=&format=webp&width=1523&height=864")
-    await ctx.send(embeds=[embed1, embed2,])
-    
     # 🔹 Footer (κάτω μέρος)
     embed.set_footer(text="🔥 Made by Mimbos 💀")
+    
+    embed3 = discord.Embed(color=discord.Color.red())
+    embed3.set_image(url="https://media.discordapp.net/attachments/1288996430383677450/1289343395025784863/Screenshot_20240928_004928_Chrome.jpg?ex=69028c1c&is=69013a9c&hm=cf2ef2b31140a8ee32f572c2ed48f1eec99c429e45ae030a3ef02a124a98fea8&=&format=webp&width=1523&height=864")
+    await ctx.send(embeds=[embed1, embed2, embed3])
 
     # 🔹 Τρέχουσα ώρα
     embed.timestamp = discord.utils.utcnow()
