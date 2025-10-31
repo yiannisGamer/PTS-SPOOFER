15b1014c81548e8ba8521c285eaf336ed817dfd1
yiannisGamer <83921980+yiannisGamer@users.noreply.github.com>
2025-10-31 03:31:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,6 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.lower() == "ping":
-        await message.channel.send("hello")
 # main.py - δουλειά 100% για Railway (commands.Bot + !ticket command + ping)
 import os
 import asyncio
